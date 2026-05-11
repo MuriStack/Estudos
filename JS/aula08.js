577dc92076aee46f57ae69e8b9f21e27a13192fb
belizario <murilo.belizario12@gmail.com>
2026-05-11 18:41:22 -0300

    Adição de novas aulas

---
+++ JS/aula08.js
@@ -0,0 +1,23 @@
+let n1,n2,res
+
+n1 = 10
+n2 = 11
+res = n1 & n2
+
+console.log(res)
+
+res = n1 | n2
+
+console.log(res)
+
+res = n1 ^ n2
+
+console.log(res)
+
+res = n1 << 1
+
+console.log(res)
+
+res = n1 >> 1 
+
+console.log(res)
